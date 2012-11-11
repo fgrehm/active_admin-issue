a460da6ecb1ba9620041f51ddc2d40d2bfd9cf28
Fabio Rehm <fgrehm@gmail.com>
2012-11-11 20:37:44 -0200

    New blank app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ActiveAdminIssue::Application.config.secret_token = '6c426083d15889892b358ab4fedfb0130eeb616286e7bdf939daee0b1f975cbb80b705b0c92314d83f7011b799d27d1791a2e8e676bebd0f6918ed25dfade3e2'
